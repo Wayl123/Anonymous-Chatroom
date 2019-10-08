840084455d7ee547537bf4048bc17e79d83c440a
Wayne Lee <waynel1996weiyang@gmail.com>
2019-10-08 17:54:34 -0400

    Fix small syntax oddity.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var express = require("express"),
-	app = express();
+	app = express(),
 	http = require("http").createServer(app),
 	io = require("socket.io")(http),
 	bodyParser = require("body-parser");
